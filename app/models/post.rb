758331378e869b4f1d5cfda41e62569a1ac936e3
hide1120 <takahide1120@gmail.com>
2021-02-01 21:13:49 +0900

    ログインアクション色々

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  # 共通
   validates :title, :body, :img, :urls, presence: true
 
+  # タイトル
   mount_uploader :img, ImgUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  has_secure_password
+  before_save :downcase_email
 
-  validates :email, presence: true
-  validates :password, confirmation: true
+  # email validates
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  # password validates
+  has_secure_password
+  validates :password, presence: true, confirmation: true,
+                       length: { minimum: 6 }, allow_nil: true
+
+    private
+      # メルアドをすべて小文字に変換する
+      def downcase_email
+        self.email = emial.downcase
+      end
 end
