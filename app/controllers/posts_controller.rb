b05b03c40422c71d25c9435090b23d7c2d843061
hide1120 <takahide1120@gmail.com>
2021-02-02 16:58:18 +0900

    ログインアクション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user, only: [:new, :create, :destroy]
+  before_action :authenticate_user, except: [:index, :show]
 
   def index
     @posts = Post.order(created_at: :desc)
