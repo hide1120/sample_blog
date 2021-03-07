e58effe4f9a04f4dd17742daddd1b53fe3f17c51
hide1120 <takahide1120@gmail.com>
2021-03-07 22:01:38 +0900

    更新作業]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # タイトル
   def full_title(page_title = '')
-    base_title = "ポートフォリオ"
+    base_title = "Welcome to My Portfolio - 私が担当・制作させて頂いたWebサイトのご紹介"
     if page_title.empty?
       base_title
     else
